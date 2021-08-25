e27ae7c732f43c13936e3c3dcca059003901b951
kanai1192 <kanai1192>
2021-08-25 10:15:44 +0900

    update codes

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # fed4iot-cbpf-pana
+
+## Usage  
+```bash
+$cd ~/fed4iot-cbpf-pana/src/FaceDetectionAndRecognition  
+$python3 restapi_main.py
+```
+## Test script (for TV side)
+```bash
+$cd ~/fed4iot-cbpf-pana/test_tool/
+$python3 test_cbpf.py <cmd>
+ <cmd>: start, close, get_result
+```
+
+
---
+++ test_tool/test_cbpf.py
@@ -0,0 +1,59 @@
+import requests
+import sys
+import base64
+import json
+
+REST_HOST = "192.168.11.125:5000"
+API = "/api"
+
+testImg = "test.jpg"
+
+headers = {'Content-Type': 'application/octet-stream'}
+
+def callReadImg():
+
+    with open(testImg, 'rb') as f:
+        srcImg = f.read()
+    
+    binImg = base64.b64encode(srcImg).decode('utf-8')
+
+    up_img = {"content": {"value": binImg},
+              "file name": {"value": testImg}}
+
+    return up_img
+
+def main():
+
+    args = sys.argv
+    
+    if (len(args) != 2):
+        print ("[Usage] python3 **.py <cmd>")
+        print ("start, close, upload, get_result")
+        sys.exit(1)
+
+    if args[1] == "upload":
+        cmd = "/upload"
+        headers = {'Content-Type': 'application/json'}
+        data = callReadImg()
+        data = json.dumps(data)
+        end_point = 'http://' + REST_HOST + API + str(cmd)
+        response = requests.post(end_point, headers=headers, data=data)
+
+    elif args[1] == "get_result":
+        cmd = "/get_result"
+        end_point = 'http://' + REST_HOST + API + str(cmd)
+        response = requests.get(end_point)
+
+    else:
+        cmd = "/face_rec=" + args[1]
+        #headers = {'Content-Type': 'text/plain'}
+        end_point = 'http://' + REST_HOST + API + str(cmd)
+        response = requests.get(end_point)
+
+    print (response.json())
+
+
+if __name__ == '__main__':
+
+    main()
+
