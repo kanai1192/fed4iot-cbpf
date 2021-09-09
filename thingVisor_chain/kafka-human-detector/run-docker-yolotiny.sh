e931a0eb7bcbc766990bd0dfd96dc62ba0999f68
kanai1192 <k.kanai@aoni.waseda.jp>
2021-09-09 16:00:47 +0900

    add thingvisor chain

--- thingVisor_chain/kafka-human-detector/run-docker-yolotiny.sh
+++
@@ -1,6 +0,0 @@
-NAME="iot-a-kafka-yolotiny"
-TOPIC="kakfa:/yolotiny/dummy"
-BROKER="133.9.250.211:9092"
-CONTROL="133.9.250.223:9092"
-
-docker run --name ${NAME} --env TOPIC=${TOPIC} --env BROKER=${BROKER} --env CONTROL=${CONTROL} -d -it kanai1192/${NAME}
